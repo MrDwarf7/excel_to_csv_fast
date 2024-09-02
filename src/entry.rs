d95573cbe40347b9307f5a4272978350a5e0d3f8
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2024-09-02 14:42:10 +1000

    perf(threads): Remoe thread sleep

--- src/entry.rs
+++ src/entry.rs
@@ -1,5 +1,5 @@
 use crate::{convert::convert, Result};
-use std::{ops::DerefMut, path::PathBuf, sync::mpsc::Receiver};
+use std::{path::PathBuf, sync::mpsc::Receiver};
 
 pub fn run() -> Result<()> {
     let start = std::time::Instant::now();
@@ -19,20 +19,19 @@ pub fn run() -> Result<()> {
 
     // let (timer_tx, timer_rx) = std::sync::mpsc::channel();
     // tasks.push(toggle_timer(timer_rx));
-    //
     // timer_tx.send(TimerLife::Start).unwrap();
 
     let (tx, rx) = std::sync::mpsc::channel();
+
     for file in files {
         let tx = tx.clone();
         tasks.push(std::thread::spawn(move || {
-            let mut file = file.clone();
-            let file = file.deref_mut();
+            // let mut file = file.clone();
+            // let file = file.deref_mut();
             convert(file.to_path_buf()).unwrap();
             tx.send(()).unwrap();
-            std::thread::sleep(std::time::Duration::from_millis(50));
+            // std::thread::sleep(std::time::Duration::from_millis(50));
         }));
-        // convert(&file)?;
     }
 
     tasks.push(std::thread::spawn(move || {
@@ -43,8 +42,6 @@ pub fn run() -> Result<()> {
     }));
     drop(tx);
 
-    // timer_tx.send(TimerLife::Stop).unwrap();
-    //
     for task in tasks {
         task.join().unwrap();
     }
@@ -53,63 +50,6 @@ pub fn run() -> Result<()> {
     println!("Time elapsed: {:?}", end);
 
     Ok(())
-}
-
-pub enum TimerLife {
-    Start,
-    Stop,
-}
-
-// Not working as intended lol
-pub fn toggle_timer(rx_timer: Receiver<TimerLife>) -> std::thread::JoinHandle<()> {
-    let mut timer: Timer = Timer::start();
-
-    std::thread::spawn(move || loop {
-        match rx_timer.recv() {
-            Ok(TimerLife::Start) => {
-                timer = Timer::start();
-                println!("Timer started");
-            }
-            Ok(TimerLife::Stop) => {
-                timer.stop();
-                println!("Timer stopped: {:?}", timer.store_time.unwrap());
-                break;
-            }
-            _ => (),
-        }
-    })
-}
-
-struct Timer {
-    start: std::time::Instant,
-    store_time: Option<std::time::Duration>,
-}
-
-impl Timer {
-    pub fn start() -> Self {
-        Self {
-            start: std::time::Instant::now(),
-            store_time: None,
-        }
-    }
-
-    pub fn stop(&mut self) {
-        let el = self.start.elapsed();
-        self.store_time = Some(el);
-    }
-}
-
-pub fn args_or_input() -> Result<PathBuf> {
-    let args: Vec<String> = std::env::args().collect();
-    if args.len() > 1 {
-        Ok(PathBuf::from(args[1].clone()))
-    } else {
-        println!("Please provide the path to the excel file: ");
-        // print!("Please provide the path to the excel file: ");
-        let mut input = String::new();
-        std::io::stdin().read_line(&mut input)?;
-        Ok(PathBuf::from(input.trim()).canonicalize()?)
-    }
 }
 
 pub fn filter_for_compat(dir_with_files: PathBuf) -> Result<Vec<PathBuf>> {
@@ -125,3 +65,60 @@ pub fn filter_for_compat(dir_with_files: PathBuf) -> Result<Vec<PathBuf>> {
     }
     Ok(files)
 }
+
+pub fn args_or_input() -> Result<PathBuf> {
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() > 1 {
+        Ok(PathBuf::from(args[1].clone()))
+    } else {
+        println!("Please provide the path to the excel file: ");
+        // print!("Please provide the path to the excel file: ");
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input)?;
+        Ok(PathBuf::from(input.trim()).canonicalize()?)
+    }
+}
+
+// struct Timer {
+//     start: std::time::Instant,
+//     store_time: Option<std::time::Duration>,
+// }
+//
+// impl Timer {
+//     pub fn start() -> Self {
+//         Self {
+//             start: std::time::Instant::now(),
+//             store_time: None,
+//         }
+//     }
+//
+//     pub fn stop(&mut self) {
+//         let el = self.start.elapsed();
+//         self.store_time = Some(el);
+//     }
+// }
+//
+// pub enum TimerLife {
+//     Start,
+//     Stop,
+// }
+//
+// // Not working as intended lol
+// pub fn toggle_timer(rx_timer: Receiver<TimerLife>) -> std::thread::JoinHandle<()> {
+//     let mut timer: Timer = Timer::start();
+//
+//     std::thread::spawn(move || loop {
+//         match rx_timer.recv() {
+//             Ok(TimerLife::Start) => {
+//                 timer = Timer::start();
+//                 println!("Timer started");
+//             }
+//             Ok(TimerLife::Stop) => {
+//                 timer.stop();
+//                 println!("Timer stopped: {:?}", timer.store_time.unwrap());
+//                 break;
+//             }
+//             _ => (),
+//         }
+//     })
+// }
